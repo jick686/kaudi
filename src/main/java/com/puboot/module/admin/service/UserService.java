a8f4fe0a92f55306943abb9daed97a8e100556e4
wujiazhong <1345651620@qq.com>
2020-04-25 11:20:44 +0800

    mod 测试提交  wjz 2020.4.25

--- src/main/java/com/puboot/module/admin/service/UserService.java
+++ src/main/java/com/puboot/module/admin/service/UserService.java
@@ -9,9 +9,9 @@ import java.io.Serializable;
 import java.util.List;
 
 /**
- * @author Linzhaoguan
+ * @author wjz
  * @version V1.0
- * @date 2019年9月11日
+ * @date 2020.4.25
  */
 public interface UserService extends IService<User> {
 
